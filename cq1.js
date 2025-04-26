b0108022cd2801f4f713df94c359f1bd9b8c9382
Abhilash Reddy <iamabhi31ind@gmail.com>
2025-04-26 15:39:07 +0530

    Coding ques 1 and solution

--- cq1.js
+++ cq1.js
@@ -1,3 +1,15 @@
+Coding Question 1: The Encapsulated Counter Factory
+
+Write a function called createUniqueCounter that, when invoked, returns a new counter object. Each counter object should have two methods:
+
+●	increment(): Increments an internal counter variable (initially 0) and returns the new value.
+●	getValue(): Returns the current value of the internal counter variable without modifying it.
+  
+Ensure that the internal counter variable is not directly accessible or modifiable from outside the counter object. Demonstrate the creation and usage of two independent counter objects created by createUniqueCounter.
+
+  
+
+
 function createUniqueCounter() {
   let count = 0;
 
