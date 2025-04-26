63bf2cc230cf296f0bee3f3a6e9c86ec925803a0
Abhilash Reddy <iamabhi31ind@gmail.com>
2025-04-26 15:39:57 +0530

    Coding ques 2 and solution

--- cq2.js
+++ cq2.js
@@ -1,3 +1,16 @@
+Coding Question 2: The Delayed Object Processor
+
+You are given an array of objects, where each object has a name property and a process method. Write a function called processObjectsDelayed that takes this array and a delay in milliseconds. For each object in the array, after the specified delay, the process method of that object should be called, and it should log a message to the console including the object's name. Be mindful of the this context when calling the process method.
+  
+Example input array:
+JavaScript
+const items = [
+  { name: "Alpha", process: function() { console.log(`Processing: ${this.name}`); } },
+  { name: "Beta", process: function() { console.log(`Task for ${this.name} done.`); } }
+];
+
+
+//Solution
 function processObjectsDelayed(items, delay) {
   items.forEach(item => {
     setTimeout(() => {
